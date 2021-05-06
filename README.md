51738a6229610fa071f239f26f246867f918b355
ygorazambuja <ygorazambuja@gmail.com>
2021-05-06 19:07:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+- Take Bli API
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import axios from "axios";
+import { RootObject } from "./interfaces/RootObject";
+
+const app = express();
+
+app.get("/github", async (_, res) => {
+  const { data } = await axios.get<RootObject[]>(
+    "https://api.github.com/users/takenet/repos?sort=created&direction=asc"
+  );
+
+  const getOnlyCSharp = data.filter((repo) => repo.language === "C#");
+
+  const array = [];
+  for (let i = 0; i < 5; i++) {
+    array.push(getOnlyCSharp[i]);
+  }
+
+  res.json(array);
+});
+
+app.listen(3000);
